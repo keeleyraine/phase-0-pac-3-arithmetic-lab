df6616542107c2262a9530575c7442c942ffcfd9
Keeley Madigan <keeleysprouts@gmail.com>
2023-12-08 22:28:54 -0800

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a+b
+};
+function subtract(a,b) {
+    return a-b
+};
+function multiply (a,b) {
+    return a*b
+};
+function divide (a,b) {
+return a/b
+};
+let n = 69
+function increment(n) {
+    return ++n
+};
+function decrement(n) {
+    return --n
+};
+function makeInt(n,b) {
+    parseInt(`Nonsense!`,10)
+};
+function makeInt(n,b) {
+ return parseInt(n, 10)
+};
+function preserveDecimal(n) {
+return parseFloat(n,10)
+};
